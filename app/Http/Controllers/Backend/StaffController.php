f6abb3f2f025b44521ceef55a52538fb61eba89a
husnilk <husnil.k@gmail.com>
2019-10-05 10:33:57 +0700

    finished StaffController

--- app/Http/Controllers/Backend/StaffController.php
+++ app/Http/Controllers/Backend/StaffController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Staff;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StaffController extends Controller
 {
@@ -19,69 +21,86 @@ class StaffController extends Controller
         return view('backend.staff.index', compact('staffs'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('backend.staff.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, Staff::$validation_rules);
+
+        $user = User::create([
+            'username' => request('nip'),
+            'email' => request('email'),
+            'password' => bcrypt('nip'),
+            'status' => 1,
+            'type' => User::STAFF
+        ]);
+
+        if ($request->file('photo')->isValid()) {
+            $filename = uniqid('staff-');
+            $fileext = $request->file('photo')->extension();
+            $filenameext = $filename.'.'.$fileext;
+
+            $filepath = $request->photo->storeAs(config('central.folder.staff_photo'), $filenameext);
+        }
+
+        $data=$request->except('photo', 'email');
+        $data['photo'] = $filenameext;
+        $user->staff()->create($data);
+
+        session()->flash('flash_success', 'Berhasil menambahkan data tendik atas nama '. $request->input('name'));
+        return redirect()->route('admin.staff.show', [$user->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Staff  $staff
-     * @return \Illuminate\Http\Response
-     */
     public function show(Staff $staff)
     {
-        //
+        return view('backend.staff.show', compact('tendik'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Staff  $staff
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Staff $staff)
     {
-        //
+        return view('backend.staff.edit', compact('staff'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Staff  $staff
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Staff $staff)
     {
-        //
+        $this->validate($request, Staff::$validation_rules);
+
+        $staff->user->update([
+            'password' => bcrypt('secret'),
+            'email' => request('email'),
+            'status' => 1,
+        ]);
+
+        if ($request->file('photo')->isValid()) {
+
+            Storage::disk('public')->delete(config('central.folder.staff_photo').$staff->photo);
+
+            $filename = uniqid('staff-');
+            $fileext = $request->file('photo')->extension();
+            $filenameext = $filename.'.'.$fileext;
+
+            $filepath = $request->photo->storeAs(config('central.folder.staff_photo'), $filenameext);
+        }
+        $data=$request->except('photo', 'email');
+        $data['photo'] = $filenameext;
+        $staff->update($data);
+
+        session()->flash('flash_success', 'Berhasil mengupdate data tendik '.$staff->name);
+        return redirect()->route('admin.staff.show', [$staff->id]);
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Staff  $staff
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Staff $staff)
     {
-        //
+        $user = User::find($staff->id);
+        $staff->delete();
+
+        optional($user)->delete();
+
+        session()->flash('flash_success', 'Berhasil menghapus tendik '.$staff->name);
+        return redirect()->route('admin.staff.index');
     }
 }
---
+++ config/central.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'default_paginate_item' => 50,
+
+    'folder' => [
+        'staff_photo' => 'public/photo/tendik'
+    ]
+];
